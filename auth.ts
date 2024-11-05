93d838341e14926164122ae2df18b063c786680f
Elcale1703 <m.agudeloo@uniandes.edu.co>
2024-11-05 12:21:23 -0500

    Seguir ahora

--- auth.ts
+++ auth.ts
@@ -1,7 +1,73 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
- 
+import { client } from "./sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "./sanity/lib/queries";
+import { writeClient } from "./sanity/lib/write-client";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
-})
+  providers: [
+    GitHub,
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === "github") {
+        // Lógica para GitHub
+        const { name, email, image } = user;
+        const { id, login, bio } = profile;
+
+        const existingUser = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_ID_QUERY, {
+            id,
+          });
+
+        if (!existingUser) {
+          await writeClient.create({
+            _type: "author",
+            id,
+            name,
+            username: login,
+            email,
+            image,
+            bio: bio || "",
+          });
+        }
+        return true;
+      } else if (account.provider === "google") {
+        // Lógica específica para Google
+        return profile.email_verified && profile.email.endsWith("@example.com");
+      }
+
+      return true; // Permitir otros proveedores si se agregan en el futuro
+    },
+
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
+});
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -31,8 +31,19 @@ export const STARTUP_BY_ID_QUERY =
         pitch,
     }`);
 
-export const STARTUP_VIEWS_QUERY = 
+export const STARTUP_VIEWS_QUERY =
     defineQuery(`*[_type == "startup" && _id == $id][0] {
         _id,
         views
+    }`);
+
+export const AUTHOR_BY_ID_QUERY =
+    defineQuery(`*[_type == "author" && id == $id][0] {
+        _id,
+        id,
+        name,
+        username,
+        email,
+        image,
+        bio
     }`)
